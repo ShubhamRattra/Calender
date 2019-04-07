0851a0dbe53f855c700fe4b79529a93a80bb4606
Shubham Rattra <shubhamrattra@gmail.com>
2019-04-07 23:39:46 +0530

    first commit

---
+++ app/src/main/java/com/shubham/calender/MainActivity.java
@@ -0,0 +1,31 @@
+package com.shubham.calender;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView ;
+    TextView myDate ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calenderView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1+1)+"/"+i2+"/"+i ;
+                myDate.setText(date);
+            }
+        });
+
+    }
+}
